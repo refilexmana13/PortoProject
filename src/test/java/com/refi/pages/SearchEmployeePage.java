2a327cbf96196cef4677c96df7384e2811c02922
Refi Lexmana <refi.lexmana13@gmail.com>
2026-06-03 07:14:39 +0700

    change the xpath for input Employee Name at SearchEmployee

--- src/test/java/com/refi/pages/SearchEmployeePage.java
+++ src/test/java/com/refi/pages/SearchEmployeePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class SearchEmployeePage extends BasePage {
 
     private final By pageTitle = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[1]/h5");
-    private final By inputSearchName = By.xpath("//input[@placeholder='Type for hints...']");
+    private final By inputSearchName = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input");
     private final By btnSearch = By.xpath("//button[@type='submit']");
     private final By resultList = By.xpath("//div[@class='oxd-table-body']//div[@class='oxd-table-card']");
 
