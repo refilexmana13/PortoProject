7642e4b674d752c9d899cf8e22cd4cbe541f38c9
Refi Lexmana <refi.lexmana13@gmail.com>
2026-06-03 07:06:51 +0700

    Add pageTittle at SearchEmployeePage

--- src/test/java/com/refi/pages/SearchEmployeePage.java
+++ src/test/java/com/refi/pages/SearchEmployeePage.java
@@ -2,14 +2,17 @@ package com.refi.pages;
 
 import com.refi.base.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SearchEmployeePage extends BasePage {
 
+    private final By pageTitle = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[1]/h5");
     private final By inputSearchName = By.xpath("//input[@placeholder='Type for hints...']");
     private final By btnSearch = By.xpath("//button[@type='submit']");
     private final By resultList = By.xpath("//div[@class='oxd-table-body']//div[@class='oxd-table-card']");
 
     public void enterSearchName(String name) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pageTitle));
         type(inputSearchName, name);
     }
 
